94a85fb271d0974926d9f08ffe051a85cf2383ed
Md Enamul  Haque <enamulhaquecoder@gmail.com>
2023-08-07 10:22:34 +0600

    vendor profile

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -15,6 +15,21 @@
         </li>
         <li class="menu-header">Starter</li>
 
+           {{-- Manage Categories Start --}}
+
+           <li class="dropdown {{ setActive([
+            'admin.category.*',
+            'admin.sub-category.*',
+            'admin.child-category.*',
+          ]) }}">
+            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Ecommerce</span></a>
+            <ul class="dropdown-menu">
+              <li class="{{ setActive(['admin.category.*']) }}"><a class="nav-link" href="{{ route('admin.category.index') }}">Vendor Profile</a></li>
+            </ul>
+          </li>
+  
+          {{-- Manage Categories end --}}
+
 
         
         {{-- Manage Categories Start --}}
